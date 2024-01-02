468ccf11a47503ff79e6d8d63178ab050b27c824
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2024-01-02 12:53:54 +0100

    Ajout des classes de filtres

---
+++ strategy_1/src/filter/BlackAndWhiteFilter.java
@@ -0,0 +1,8 @@
+package filter;
+
+public class BlackAndWhiteFilter implements Filter{
+    @Override
+    public void apply() {
+        System.out.println("Filter Black and White");
+    }
+}
---
+++ strategy_1/src/filter/HighContrastFilter.java
@@ -0,0 +1,8 @@
+package filter;
+
+public class HighContrastFilter implements Filter{
+    @Override
+    public void apply() {
+        System.out.println("Filter High Contrast");
+    }
+}
